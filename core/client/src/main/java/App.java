c01a3056533dedb063621c875f140bd0fe98f06c
HuaJuan <zhj630985214@gmail.com>
2021-11-17 22:48:15 +0800

    之前在server的pasv和client的port中用了不靠谱的InetAddress.getlocalhost方法，在非Windows上很容易返回127.0.0.1，现在改用遍历NetworkInterface

--- core/client/src/main/java/App.java
+++ core/client/src/main/java/App.java
@@ -3,18 +3,25 @@ import core.MyFTPClientCore;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
 import java.net.Socket;
+import java.util.Enumeration;
+import java.util.Objects;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        MyFTPClientCore myFTPClientCore = null;
-        myFTPClientCore = new MyFTPClientCore("127.0.0.1", 3334);
-        boolean loginSuccess = myFTPClientCore.login("test", "test");
-        myFTPClientCore.pasv();
-        myFTPClientCore.type(MyFTPClientCore.ASCIIBinary.BINARY);
-        myFTPClientCore.setDownloadDirectory("D:\\ftp_download");
-        myFTPClientCore.kali(MyFTPClientCore.KeepAlive.T);
-        myFTPClientCore.storeSingleFile("D:\\huajuanFTP", "咩咩咩");
+//        MyFTPClientCore myFTPClientCore = null;
+//        myFTPClientCore = new MyFTPClientCore("127.0.0.1", 3334);
+//        boolean loginSuccess = myFTPClientCore.login("test", "test");
+//        myFTPClientCore.pasv();
+//        myFTPClientCore.type(MyFTPClientCore.ASCIIBinary.BINARY);
+//        myFTPClientCore.setDownloadDirectory("D:\\ftp_download");
+//        myFTPClientCore.kali(MyFTPClientCore.KeepAlive.T);
+//        myFTPClientCore.storeSingleFile("D:\\huajuanFTP", "咩咩咩");
+
 
     }
+
+
 }
--- core/server/src/main/java/server/core/command/concrete/PASVCommand.java
+++ core/server/src/main/java/server/core/command/concrete/PASVCommand.java
@@ -8,8 +8,10 @@ import server.core.thread.HandleUserRequestThread;
 
 import java.io.IOException;
 import java.net.InetAddress;
+import java.net.NetworkInterface;
 import java.net.ServerSocket;
 import java.security.SecureRandom;
+import java.util.Enumeration;
 
 public class PASVCommand extends AbstractCommand {
     public PASVCommand(String commandType, String commandArg) {
@@ -52,7 +54,9 @@ public class PASVCommand extends AbstractCommand {
             openServerSocketSuccess = true;
 
             //获取本机的地址
-            InetAddress localAddress = InetAddress.getLocalHost();
+            InetAddress localAddress = PASVUtils.getLocalHostExactAddress();
+
+            assert localAddress != null;
 
             //返回给用户的response
             AbstractResponse rsp = new EnteringPassiveModeResponse(localAddress.getHostAddress(), p1, p2);
@@ -70,5 +74,41 @@ public class PASVCommand extends AbstractCommand {
             handleUserRequestThread.writeLine(rsp.toString());
 
         } while (!openServerSocketSuccess);
+    }
+}
+
+class PASVUtils {
+    public static InetAddress getLocalHostExactAddress() {
+        try {
+            InetAddress candidateAddress = null;
+
+            Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+            while (networkInterfaces.hasMoreElements()) {
+                NetworkInterface iface = networkInterfaces.nextElement();
+                // 该网卡接口下的ip会有多个，也需要一个个的遍历，找到自己所需要的
+                for (Enumeration<InetAddress> inetAddrs = iface.getInetAddresses(); inetAddrs.hasMoreElements(); ) {
+                    InetAddress inetAddr = inetAddrs.nextElement();
+                    // 排除loopback回环类型地址（不管是IPv4还是IPv6 只要是回环地址都会返回true）
+                    if (!inetAddr.isLoopbackAddress()) {
+                        if (inetAddr.isSiteLocalAddress()) {
+                            // 如果是site-local地址，就是它了 就是我们要找的
+                            // ~~~~~~~~~~~~~绝大部分情况下都会在此处返回你的ip地址值~~~~~~~~~~~~~
+                            return inetAddr;
+                        }
+
+                        // 若不是site-local地址 那就记录下该地址当作候选
+                        if (candidateAddress == null) {
+                            candidateAddress = inetAddr;
+                        }
+
+                    }
+                }
+            }
+
+            // 如果出去loopback回环地之外无其它地址了，那就回退到原始方案吧
+            return candidateAddress == null ? InetAddress.getLocalHost() : candidateAddress;
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
